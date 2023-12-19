337c7f7492a63d3b7d8a81cd4746d6b9fbc1e4a4
marian.kuziv <mi.kuziv@gmail.com>
2023-12-19 16:40:14 +0200

    Adding new kafka-streams module

---
+++ kafka-streams-wikimedia/src/main/java/edu/jmarkuz/training/KafkaStreamsWikimediaApplication.java
@@ -0,0 +1,13 @@
+package edu.jmarkuz.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsWikimediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamsWikimediaApplication.class, args);
+    }
+
+}
